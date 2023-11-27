fcf6599abfcfc1d892c38d64c49f685eb6ae9574
QureshiYousuf <yousuf337692qureshi@gmail.com>
2023-11-27 11:34:23 +0530

    final commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NodeJS-Placement-Cell
+
  <h1 align="center">Placement Cell  📝</h1> 
 <p align="center">
  🖊️ An interface for coding ninjas placement cell to keep records of students and schedule interviews with different companies <br>
@@ -12,26 +13,6 @@
 - Allocate and schedule interview with different companies and update their result status
 - Download reports of students in csv format
 
-## Screenshots
-
-- Sign Up
-  ![Sign-Up](./images/signup.PNG)
-
-- Sign In
-  ![Sign-In](./images/login.PNG)
-
-- Student Page
-  ![Student-Page](./images/studentlist.PNG)
-
-- Interview Page
-  ![Interview-Page](./images/companylist.PNG)
-
-- Add student
-  ![Add-Student](./images/AddStudent.PNG)
-
-- Schedule Interview
-  ![Interview](./images/InterviewSchedule.PNG)
-
 ## Setup
 
 Run `npm install` to install required dependencies
@@ -41,5 +22,3 @@ Environment Variables:
 - PORT = [Your Port]
 - SECRET = [Your Secret]
 - MONGO_URI = [Your MongoDB Ur]
-
-<!-- ATLAS - bH764IQfTC3MYtbK -->
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //connecting mongoose with database
-const DB = 'mongodb+srv://yousuf337692qureshi:bH764IQfTC3MYtbK@cluster0.cvff5s1.mongodb.net/placement-cell?retryWrites=true&w=majority';
+const DB =
+  "mongodb+srv://yousuf337692qureshi:bH764IQfTC3MYtbK@cluster0.cvff5s1.mongodb.net/placement-cell?retryWrites=true&w=majority";
 
 mongoose.connect(DB, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // for local use, use the below connection
@@ -13,10 +14,10 @@ mongoose.connect(DB, {
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+db.on("error", console.error.bind(console, "Error in connecting to MongoDB"));
 
-db.once('open', function () {
-	console.log('Connected to Database :: Mongodb');
+db.once("open", function () {
+  console.log("Connected to Database :: Mongodb");
 });
 
 module.exports = mongoose;
