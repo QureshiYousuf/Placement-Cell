051be9e4f71f889f5e0cd13e56029aca6b483833
QureshiYousuf <yousuf337692qureshi@gmail.com>
2023-11-29 10:36:31 +0530

    final commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 //connecting mongoose with database
-const DB =
-  "mongodb+srv://yousuf337692qureshi:bH764IQfTC3MYtbK@cluster0.cvff5s1.mongodb.net/placement-cell?retryWrites=true&w=majority";
+// const DB =
+//   "mongodb+srv://yousuf337692qureshi:bH764IQfTC3MYtbK@cluster0.cvff5s1.mongodb.net/placement-cell?retryWrites=true&w=majority";
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(DB, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 // for local use, use the below connection
-// mongoose.connect('mongodb://0.0.0.0:27017/placement_cell');
+mongoose.connect("mongodb://localhost:27017/placement_cell");
 
 const db = mongoose.connection;
 
